60f0bd84d61edea45a9e0f067eecd3079605a035
gabriel <gbp@ic.ufal.br>
2021-03-18 23:56:22 -0300

    construct collapsible container to windows

--- new_group.js
+++ new_group.js
@@ -1,22 +1,67 @@
-/* configure collapsible */
-let coll = document.getElementsByClassName("collapsible");
-for (let i = 0; i < coll.length; i++) {
-    coll[i].addEventListener("click", function() {
+// references to html elements
+let contentBody = document.getElementsByClassName("content-body")[0];
+
+chrome.windows.getAll({populate:true}, windows => {
+    for (let x = 0; x < windows.length; x++) {
+        let newWindow = document.createElement("div");
+        newWindow.classList.add("window");
+        for (let z = 0; z < windows[x].tabs.length; z++) {
+            let tab = constructTab();
+        }
+        let button = constructCollapsibleButton("Window "+ (x+1));
+        contentBody.appendChild(button);
+        contentBody.appendChild(newWindow)
+    }
+});
+
+function constructCollapsibleButton(name) {
+    // creating collapsible button
+    let collapsibleButton = document.createElement("button");
+    collapsibleButton.classList.add("collapsible");
+
+    // adding collapsible behaviour
+    collapsibleButton.addEventListener("click", function() {
+        console.log('oi', this);
         this.classList.toggle("active");
-        var content = this.nextElementSibling;
+        let content = this.nextElementSibling;
         if (content.style.maxHeight){
             content.style.maxHeight = null;
         } else {
             content.style.maxHeight = content.scrollHeight + "px";
         }
     });
+
+    // creating checkbox
+    let checkbox = document.createElement("input");
+    checkbox.type = "checkbox";
+    checkbox.name = "windowCheckbox";
+    checkbox.id = name.replace(/ /g, '');
+
+    // add name and checkbox to button
+    collapsibleButton.appendChild(checkbox);
+    collapsibleButton.innerHTML += name;
+
+    return collapsibleButton;
 }
 
-chrome.windows.getAll({populate:true}, windows => {
-    for (let i = 0; i < windows.length; i++) {
-        // todo: construct the html of windows
-        for (let j = 0; j < windows[i].tabs.length; j++) console.log('oi')
-            // todo: construct the html of tabs
-    }
-});
+function constructTab() {
+    let tab = document.createElement("div");
+    tab.classList.add("tab", "group");
 
+    return tab;
+}
+
+
+// let coll = document.getElementsByClassName("collapsible");
+// for (let i = 0; i < coll.length; i++) {
+//     coll[i].addEventListener("click", function() {
+//         console.log(this);
+//         this.classList.toggle("active");
+//         var content = this.nextElementSibling;
+//         if (content.style.maxHeight){
+//             content.style.maxHeight = null;
+//         } else {
+//             content.style.maxHeight = content.scrollHeight + "px";
+//         }
+//     });
+// }
