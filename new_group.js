ad5e55c35aaf1ef5368fc4cf7a84eadd8004110a
gabriel <gbp@ic.ufal.br>
2021-03-19 12:25:49 -0300

    add checkboxes events

--- new_group.js
+++ new_group.js
@@ -3,39 +3,72 @@ let contentBody = document.getElementsByClassName("content-body")[0];
 
 chrome.windows.getAll({populate:true}, windows => {
     for (let x = 0; x < windows.length; x++) {
+        // if (x > 0) return;
         let button = constructCollapsibleButton("Window "+ (x+1));
         contentBody.appendChild(button);
         let newWindow = document.createElement("div");
         newWindow.classList.add("window");
         for (let z = 0; z < windows[x].tabs.length; z++) {
-            let tab = constructTabList(windows[x].tabs[z]);
+            let tab = constructTabList(windows[x].tabs[z], (x + 1));
             newWindow.appendChild(tab);
         }
         contentBody.appendChild(newWindow)
     }
+    addCheckboxesEvent();
 });
+
+function addCheckboxesEvent() {
+    let windowCheckbox = document.querySelectorAll("input[type=checkbox][name=windowCheckbox]");
+
+    // add change event to window checkbox
+    for (let idx = 1; idx <= windowCheckbox.length; idx++) {
+        let checkbox = windowCheckbox[idx - 1];
+        checkbox.addEventListener('change', function () {
+            let query = "input[type=checkbox][name=tabCheckbox_" + idx + "]";
+            let tabCheckbox = document.querySelectorAll(query);
+            tabCheckbox.forEach(function (tabCheckbox) {
+                tabCheckbox.checked = checkbox.checked;
+            });
+        });
+    }
+
+    let selectAllCheckbox = document.getElementsByClassName("select-all")[0];
+    let allCheckboxes = document.querySelectorAll("input[type=checkbox]");
+
+    // add events to select all
+    selectAllCheckbox.addEventListener("click", function () {
+        allCheckboxes.forEach(function (checkbox) {
+            if (selectAllCheckbox.checked === true) {
+                checkbox.checked = true;
+            } else {
+                checkbox.checked = false;
+            }
+        });
+    });
+}
 
 function constructCollapsibleButton(name) {
     // creating collapsible button
-    let collapsibleButton = document.createElement("button");
+    let collapsibleButton = document.createElement("div");
     collapsibleButton.classList.add("collapsible");
 
     // adding collapsible behaviour
-    collapsibleButton.addEventListener("click", function() {
+    collapsibleButton.addEventListener("click", function (e) {
         this.classList.toggle("active");
         let content = this.nextElementSibling;
-        if (content.style.maxHeight){
+        if (content.style.maxHeight) {
             content.style.maxHeight = null;
         } else {
             content.style.maxHeight = content.scrollHeight + "px";
         }
-    });
+    }, false);
 
     // creating checkbox
     let checkbox = document.createElement("input");
     checkbox.type = "checkbox";
     checkbox.name = "windowCheckbox";
     checkbox.id = name.replace(/ /g, '');
+    checkbox.value = name.replace(/ /g, '');
 
     // add name and checkbox to button
     collapsibleButton.appendChild(checkbox);
@@ -44,7 +77,7 @@ function constructCollapsibleButton(name) {
     return collapsibleButton;
 }
 
-function constructTabList(tab) {
+function constructTabList(tab, windowId) {
     // creating tab div
     let tabHtml = document.createElement("div");
     tabHtml.classList.add("tab", "group");
@@ -52,7 +85,7 @@ function constructTabList(tab) {
     // creating checkbox
     let checkbox = document.createElement("input");
     checkbox.type = "checkbox";
-    checkbox.name = "tabCheckbox";
+    checkbox.name = "tabCheckbox_" + windowId;
     checkbox.id = name.replace(/ /g, '');
 
     // creating favicon
@@ -89,3 +122,9 @@ function constructTabList(tab) {
 
     return tabHtml;
 }
+
+// todo: fix checkbox inside button issue
+// todo: behaviour of checkbox
+// todo: create Group behaviour
+// todo: layout of collapsibles
+// todo: testar as permissões no manifest.json
