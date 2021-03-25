7f1555b8e881419fe7f6d555affef4edfe055d7a
gabriel <gbp@ic.ufal.br>
2021-03-25 14:33:56 -0300

    restore tabs from group

--- popup.js
+++ popup.js
@@ -5,17 +5,16 @@ let contentBody = document.getElementsByClassName("content-body")[0];
 
 
 function updateGroupList() {
+    // clean content body
     contentBody.textContent = '';
 
     // get existing groups
     chrome.storage.local.get({groups: []}, data => {
         for (let i = 0; i < data.groups.length; i++) {
-            let groupName = data.groups[i].name;
-            console.log(groupName);
-            const newGroup = constructGroup(groupName);
-            console.log(newGroup);
+            let group = data.groups[i];
+            const newGroup = constructGroup(group);
+            console.log(data.groups[i]);
             contentBody.appendChild(newGroup);
-
             // alert(JSON.stringify(groups[i], null, '  '));
         }
     })
@@ -26,7 +25,6 @@ function openNewGroupModal() {
         focused: true,
         type: "popup",
         url: "new_group.html",
-        // left: screen.width/2,
         height: 600,
         width: 600
     });
@@ -34,7 +32,7 @@ function openNewGroupModal() {
 
 
 // construct the group html
-function constructGroup(name) {
+function constructGroup(group) {
     // creating group
     let newGroup = document.createElement("div");
     newGroup.classList.add("group");
@@ -43,7 +41,7 @@ function constructGroup(name) {
     // adding group name
     let groupName = document.createElement("p");
     groupName.classList.add("group-name");
-    groupName.innerHTML += name;
+    groupName.innerHTML += group.name;
 
     // preparing restore and delete icons
     const restoreIcon = document.createElement("i");
@@ -57,13 +55,13 @@ function constructGroup(name) {
     groupRestoreButton.appendChild(restoreIcon);
     groupRestoreButton.setAttribute("title", "Restore group");
         // groupRestoreButton.addEventListener("click", restoreGroup(groups.length));
-    groupRestoreButton.addEventListener("click", restoreGroup(name));
+    groupRestoreButton.addEventListener("click", () => restoreGroup(group));
     let groupDeleteButton = document.createElement("button");
     groupDeleteButton.classList.add("only-icon", "button");
     groupDeleteButton.appendChild(deleteIcon);
     groupDeleteButton.setAttribute("title", "Delete group");
         // groupDeleteButton.addEventListener("click", deleteGroup(groups.length));
-    groupDeleteButton.addEventListener("click", deleteGroup(name));
+    groupDeleteButton.addEventListener("click", () => deleteGroup(group));
 
     // adding group elements to new group
     newGroup.appendChild(groupName);
@@ -73,12 +71,17 @@ function constructGroup(name) {
     return newGroup;
 }
 
-function restoreGroup() {
-
+function restoreGroup(group) {
+    chrome.windows.create({
+        focused: true,
+        state: "maximized",
+        type: "normal",
+        url: group.tabs,
+    });
 }
 
-function deleteGroup() {
-
+function deleteGroup(group) {
+    alert(group.name);
 }
 
 function removeAll() {
